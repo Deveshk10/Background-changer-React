882a2958861174c6ddb7f24e5b922fd65d656c42
Deveshk10 <80586334+Deveshk10@users.noreply.github.com>
2024-03-15 17:34:27 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Name from "./Name.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Name />
+
+    <App />
+  </>
+);
